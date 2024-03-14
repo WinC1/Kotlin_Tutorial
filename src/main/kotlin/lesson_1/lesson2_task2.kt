64e724b74a57ff686634a524bb51edfc7cc2ce3e
WinC <sabirzanovras@gmail.com>
2024-03-14 18:05:46 +0300

    KS-1-2 dobavlenie variables

---
+++ src/main/kotlin/lesson_1/lesson2_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main() {
+
+    var kol_zakazov:Int = 75
+    var blagodarnost:String = "Спасибо за вашу покупку"
+    println(kol_zakazov)
+    println(blagodarnost)
+
+    //var kol_rabotnikov:Int = 2000
+    //println(kol_rabotnikov)
+
+    var kol_rabotnikov:Int = 1999
+    println(kol_rabotnikov)
+
+}
