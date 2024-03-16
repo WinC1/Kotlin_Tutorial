239f3adad3edbe53310627730b49e5c876e69be5
WinC <sabirzanovras@gmail.com>
2024-03-17 00:05:43 +0300

    KS-1-2 refactor

--- src/main/kotlin/lesson_1/lesson2_task2.kt
+++ src/main/kotlin/lesson_1/lesson2_task2.kt
@@ -2,15 +2,15 @@ package org.example.lesson_1
 
 fun main() {
 
-    var kol_zakazov:Int = 75
-    var blagodarnost:String = "Спасибо за вашу покупку"
-    println(kol_zakazov)
+    val numOfOrders: Int = 75
+    val blagodarnost: String = "Спасибо за вашу покупку"
+    println(numOfOrders)
     println(blagodarnost)
 
-    //var kol_rabotnikov:Int = 2000
-    //println(kol_rabotnikov)
+    var amountOfWorkers: Int = 2000
+    //println(amountOfWorkers)
 
-    var kol_rabotnikov:Int = 1999
-    println(kol_rabotnikov)
+    amountOfWorkers = 1999
+    println(amountOfWorkers)
 
 }
