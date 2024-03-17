bf80c9c858991947c855214e44eef6e4788f2020
WinC <sabirzanovras@gmail.com>
2024-03-17 19:30:52 +0300

    KS-1-4 create data types

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_1
+
+fun main() {
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val parOfDay: String = "0.075"
+    val seconds: Short = 6480
+    val partOfYer: String = "2.0537945205479453E-4"
+    val apogee: Int = 327000
+    println("$length \n$age \n$parOfDay \n$seconds \n$partOfYer \n$apogee")
+}
